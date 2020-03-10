28798a0f67c6fff4f336f0163867d25bae12fa3b
bolbie <soul_lucky@mail.ru>
2020-03-10 16:45:29 +0400

    4.12 Fix the compatibility of the 'vsearch.py' accroding to PEP-8 using py-test with the pep8 plugin.

--- vsearch.py
+++ vsearch.py
@@ -1,10 +1,10 @@
 
-def search_for_vowels(phrase:str) -> set:
+def search_for_vowels(phrase: str) -> set:
     """Returns the vowels found in the entered phrase."""
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))
 
 
-def search_for_letters(phrase:str, letters:str='aeuio') -> set:
+def search_for_letters(phrase: str, letters: str='aeuio') -> set:
     """Return a set() of the 'letters' found in the entered 'phrase'."""
     return set(letters).intersection(set(phrase))
