0b89358f2062716e0ada4bd3f6f62725dd95942d
bolbie <soul_lucky@mail.ru>
2020-03-10 11:23:21 +0400

    4.5 Remove the variable 'found'.

--- vsearch.py
+++ vsearch.py
@@ -1,6 +1,4 @@
 def search_for_vowels(word):
     """Returns the vowels found in the entered word."""
     vowels = set('aeiou')
-    found = vowels.intersection(set(word))
-    return found
-
+    return vowels.intersection(set(word))
