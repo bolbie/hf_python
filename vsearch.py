a703db660a5d5aba57c513743d38bf7164d9f3f3
bolbie <soul_lucky@mail.ru>
2020-03-10 12:32:52 +0400

    4.8 Create an another function search_for_letters(arg1, arg2). It returns a set() of letters found in the entered phrase.

--- vsearch.py
+++ vsearch.py
@@ -1,4 +1,10 @@
+
 def search_for_vowels(phrase:str) -> set:
     """Returns the vowels found in the entered phrase."""
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))
+
+
+def search_for_letters(phrase:str, letters:str) -> set:
+    """Return a set() of the 'letters' found in the entered 'phrase'."""
+    return set(letters).intersection(set(phrase))
