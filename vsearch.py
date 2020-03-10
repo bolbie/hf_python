1632733c3cb2e662ae2abc5fa9ac15772f13ef31
bolbie <soul_lucky@mail.ru>
2020-03-10 11:47:10 +0400

    4.6 Add annotations to  document the  type of arguments and the return value of the function.

--- vsearch.py
+++ vsearch.py
@@ -1,4 +1,4 @@
-def search_for_vowels(word):
+def search_for_vowels(word:str) -> set:
     """Returns the vowels found in the entered word."""
     vowels = set('aeiou')
     return vowels.intersection(set(word))
