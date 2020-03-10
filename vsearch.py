49f39b46a5f6d33837c4ffa4a6a4c08fd66b18ee
bolbie <soul_lucky@mail.ru>
2020-03-10 11:54:20 +0400

    4.7 Rename the variable 'word' to 'phrase' to better clarify the type of argument being accepted.

--- vsearch.py
+++ vsearch.py
@@ -1,4 +1,4 @@
-def search_for_vowels(word:str) -> set:
-    """Returns the vowels found in the entered word."""
+def search_for_vowels(phrase:str) -> set:
+    """Returns the vowels found in the entered phrase."""
     vowels = set('aeiou')
-    return vowels.intersection(set(word))
+    return vowels.intersection(set(phrase))
