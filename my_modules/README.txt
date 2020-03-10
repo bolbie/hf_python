8c605efab88e261b3b58abe8fa5eab5799f19894
bolbie <soul_lucky@mail.ru>
2020-03-10 16:03:43 +0400

    4.10 Create my own module 'vsearch' consisting of two functions: search_for_vowels/search_for_letters. Use 'pip' to install my module.

---
+++ my_modules/README.txt
@@ -0,0 +1,2 @@
+Here should be a description. But I'm quiet lazy at the time of writing this text,
+so there is nothing useful here. 
---
+++ my_modules/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0',
+    description='The Head First Python Search Tools',
+    author='Bolbie',
+    author_email='soul_lucky@mail.ru',
+    url='https://github.com/bolbie/hf_python',
+    py_modules=['vsearch'],
+)
