15e546735141639c0bad0077f49515dab99c0144
bolbie <soul_lucky@mail.ru>
2020-03-15 15:16:56 +0400

    5.11 Use the variable '__name__' to verify that the program starts directly.

--- vsearch_web.py
+++ vsearch_web.py
@@ -22,4 +22,5 @@ def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to Search For Letters on the web!')
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
