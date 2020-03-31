4a2e34f5eef1921de87394dac2d0fa22180bcaab
bolbie <soul_lucky@mail.ru>
2020-03-31 14:05:17 +0400

    7.2 Changes to the 'log_request' fucntion and preparation for working with classes.

--- web_app/vsearch_web.py
+++ web_app/vsearch_web.py
@@ -1,27 +1,28 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search_for_letters
+import mysql.connector
 
 app = Flask(__name__)
 
 
 def log_request(req: 'flask_request', res: str) -> None:
     """Log details of the web request and the results in MySQL database."""
-    import mysql.connector
     dbconfig = { 'host':'127.0.0.1',
                  'user':'vsearch',
                  'password':'vsearchpasswd',
                  'database':'vsearchlogDB' }
-    conn = mysql.connector.connect(**dbconfig)
-    cursor = conn.cursor()
-    _SQL = """insert into log
+    
+    with UseDatabase(dbconfig) as cursor:
+        _SQL = """insert into log
               (phrase, letters, ip, browser_string, results)
               values
               (%s, %s, %s, %s, %s)"""
-    cursor.execute(_SQL, (req.form['phrase'],
+        cursor.execute(_SQL, (req.form['phrase'],
                           req.form['letters'],
                           req.remote_addr,
                           req.user_agent.browser,
                           res ))
+        
     conn.commit()
     cursor.close()
     conn.close()
