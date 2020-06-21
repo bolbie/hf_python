43ccd6201f6ff19a6da47f6796e4bc653d20f180
bolbie <soul_lucky@mail.ru>
2020-06-21 17:24:34 +0400

    11.7 Create a personal Exception that protects the databse connection code. Also to further catch the exception and handle it with the help of 'try/except'.

--- web_app/DBcm.py
+++ web_app/DBcm.py
@@ -1,5 +1,9 @@
 import mysql.connector
 
+class ConnectionError(Exception):
+    pass
+
+
 class UseDataBase:
 
     def __init__(self, config: dict) -> None:
@@ -7,9 +11,12 @@ class UseDataBase:
         
 
     def __enter__(self) -> 'cursor':
-        self.conn = mysql.connector.connect(**self.configuration)
-        self.cursor = self.conn.cursor()
-        return self.cursor
+        try:
+            self.conn = mysql.connector.connect(**self.configuration)
+            self.cursor = self.conn.cursor()
+            return self.cursor
+        except mysql.connector.errors.InterfaceError as err:
+            raise ConnectionError(err)
 
 
     def __exit__(self, exc_type, exc_value, exc_trace) -> None:
