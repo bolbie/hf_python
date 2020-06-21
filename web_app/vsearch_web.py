33af50fe1af02f05d1c4e3c03ea131bf4aae2649
bolbie <soul_lucky@mail.ru>
2020-06-21 15:27:58 +0400

    The latest version of the program from chapter 10.

---
+++ web_app/vsearch_web.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template, request, escape, session
+from vsearch import search_for_letters
+
+from DBcm import UseDataBase
+from checker import check_logged_in
+
+app = Flask(__name__)
+
+app.config['dbconfig'] = { 'host':'127.0.0.1',
+                           'user':'vsearch',
+                           'password':'vsearchpasswd',
+                           'database':'vsearchlogDB' }
+
+@app.route('/login')
+def do_login() -> str:
+    session['logged_in'] = True
+    return 'You are now logged in'
+
+
+@app.route('/logout')
+def do_logout() -> str:
+    session.pop('logged_in')
+    return 'You are now logged out'
+
+
+def log_request(req: 'flask_request', res: str) -> None:
+    """Log details of the web request and the results in MySQL database."""
+    with UseDataBase(app.config['dbconfig']) as cursor:
+        _SQL = """insert into log
+              (phrase, letters, ip, browser_string, results)
+              values
+              (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+                          req.form['letters'],
+                          req.remote_addr,
+                          req.user_agent.browser,
+                          res ))
+
+
+@app.route('/search_for', methods=['POST'])
+def do_search() -> 'html':
+    """Extract the posted data; perform the search; return results."""
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search_for_letters(phrase, letters))
+    log_request(request, results)
+    return render_template('results.html',
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_title=title,
+                           the_results=results)
+
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    """Display this webapp's HTML form."""
+    return render_template('entry.html',
+                           the_title='Welcome to Search For Letters on the web!')
+
+
+@app.route('/viewlog')
+@check_logged_in
+def view_the_log() -> 'html':
+    """Display the contents of the vsearchlogDB as a HTML table."""
+    contents = []
+    with UseDataBase(app.config['dbconfig']) as cursor:
+        _SQL = """select phrase, letters, ip, browser_string, results from log"""
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()
+    titles = ('Phrase', 'Letters', 'Remote Addr', 'User Agent', 'Results')
+    return render_template ('viewlog.html',
+                            the_title='View Log',
+                            the_row_titles=titles,
+                            the_data=contents)
+
+
+app.secret_key = 'BolbieHFSecretKey'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
